3d8c0c49426bd2ffde00f9a130921451bd8d242b
Pratyush Kiran Panda <abhaydaredevil2@gmail.com>
2023-02-20 17:06:56 +0530

    solution added

---
+++ Repetitions.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	
+	string s;
+	cin >> s;
+	int n = s.length();;
+	int best = 0;
+	int curr = 1;
+
+	for (int i = 0; i < n; i++) {
+		if (s[i] != s[i-1]) {
+			best = max(best, curr);
+			curr = 0;
+		}
+		curr++;
+	}
+	best = max(best, curr);
+	cout << best;
+
+	return 0;
+}	
